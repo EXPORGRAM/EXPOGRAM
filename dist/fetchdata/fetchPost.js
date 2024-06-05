2881fd3366b3cf772820c7f65dbd7b214fa49ceb
ACAZUMAH <calebazumah9@gmail.com>
2024-06-05 15:50:06 +0000

    fetch post

--- dist/fetchdata/fetchPost.js
+++ dist/fetchdata/fetchPost.js
@@ -9,9 +9,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetchPostnewPost = void 0;
-const fetchPostnewPost = () => {
+exports.fetchAllPost = void 0;
+const firestore_1 = require("firebase/firestore");
+const firebase_1 = require("../firebaseconfig/firebase");
+const fetchAllPost = () => {
     return new Promise((resolve, reject) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const usercollectionref = (0, firebase_1.collection)(firebase_1.db, "users");
+            const allusersSnapshot = yield (0, firebase_1.getDocs)(usercollectionref);
+            let allPost = [];
+            for (const userDoc of allusersSnapshot.docs) {
+                const email = userDoc.id;
+                const postsCollectionRef = (0, firebase_1.collection)(firebase_1.db, `users/${email}/Post`);
+                const postsQuery = (0, firebase_1.query)(postsCollectionRef, (0, firebase_1.orderBy)('created_at', 'desc'), (0, firestore_1.limit)(2));
+                const allpostSnapshot = yield (0, firebase_1.getDocs)(postsQuery);
+                allpostSnapshot.forEach((postDoc) => {
+                    const postData = postDoc.data(); // Explicitly type the data as 'post'
+                    allPost.push(Object.assign({}, postData));
+                });
+            }
+            resolve(allPost);
+        }
+        catch (error) {
+            reject(error);
+        }
     }));
 };
-exports.fetchPostnewPost = fetchPostnewPost;
+exports.fetchAllPost = fetchAllPost;
+// async function test() {
+//     await fetchAllPost().then((succ) =>{
+//         console.log(succ)
+//     }).catch((fail) =>{
+//         console.log(fail)
+//     })
+// }
+// test()
